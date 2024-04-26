83e2321328768a8013458e6cec472ba73ece8faa
RayeesHussain <rayeesh02@gmail.com>
2024-04-26 10:36:11 +0530

    deploy code change

--- WebApplication01/Program.cs
+++ WebApplication01/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello Github Action! live with latest code");
 
 app.Run();
